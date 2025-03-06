56536facd5a71671479f7b2d649a866a41dcb14b
Daniel English <dan.english@redocly.com>
2025-03-06 22:53:22 +0000

    Testing GA logic

--- @theme/layouts/RootLayout.tsx
+++ @theme/layouts/RootLayout.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import  ReactGA from 'react-ga4'
+
 
 import { Navbar } from '@redocly/theme/components/Navbar/Navbar';
 import { Footer } from '@redocly/theme/components/Footer/Footer';
+
 
 export type LayoutConfig = {
   children: React.ReactNode;
@@ -16,3 +19,16 @@ export function RootLayout({ children }: LayoutConfig): JSX.Element {
     </div>
   );
 }
+
+
+const TRACKING_ID = 'G-Y95BGFEPNM'
+ReactGA.initialize(TRACKING_ID);
+ReactGA.send("pageview")
+
+ReactGA.event({
+category: "My API Category",
+action: "User clicked on something",
+label: "The label of what they clicked",
+value: 98,
+nonInteraction: true
+})
